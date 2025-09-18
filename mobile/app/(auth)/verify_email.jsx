75daeeacd514d9832ba35cc3265b60ac3a56e3e8
Tnath1 <aromejonathanu@gmail.com>
2025-09-18 16:39:18 +0100

    favourites

--- mobile/app/(auth)/verify_email.jsx
+++ mobile/app/(auth)/verify_email.jsx
@@ -1,117 +1,119 @@
-// import {
-//   View,
-//   Alert,
-//   Platform,
-//   KeyboardAvoidingView,
-//   ScrollView,
-//   TextInput,
-//   TouchableOpacity,
-//   Text,
-// } from "react-native";
-// import { authStyles } from "../../assets/styles/auth.styles";
-// import { Image } from "expo-image";
-// import { COLORS } from "../../constants/colors";
-// import { useSignUp } from "@clerk/clerk-expo";
-// import { useState } from "react";
+import {
+  View,
+  Alert,
+  Platform,
+  KeyboardAvoidingView,
+  ScrollView,
+  TextInput,
+  TouchableOpacity,
+  Text,
+} from "react-native";
+import { authStyles } from "../../assets/styles/auth.styles";
+import { Image } from "expo-image";
+import { COLORS } from "../../constants/colors";
+import { useSignUp } from "@clerk/clerk-expo";
+import { useState } from "react";
 
-// const VerifyEmail = ({ email, onBack }) => {
-//   const { isLoaded, signUp, setActive } = useSignUp();
-//   const [loading, setLoading] = useState(false);
-//   const [code, setCode] = useState("");
+// Not this component is currently not used anywhere
 
-//   const handleVerification = async () => {
-//     if (!isLoaded) return;
+const VerifyEmail = ({ email, onBack }) => {
+  const { isLoaded, signUp, setActive } = useSignUp();
+  const [loading, setLoading] = useState(false);
+  const [code, setCode] = useState("");
 
-//     setLoading(true);
-//     const trimmedCode = code.trim();
-//     console.log("Entered code:", trimmedCode, "length:", trimmedCode.length);
+  const handleVerification = async () => {
+    if (!isLoaded) return;
 
-//     console.log("signUp.id", signUp?.id);
-//     // return;
+    setLoading(true);
+    const trimmedCode = code.trim();
+    console.log("Entered code:", trimmedCode, "length:", trimmedCode.length);
 
-//     try {
-//       const signUpAttempt = await signUp.attemptEmailAddressVerification({
-//         code: trimmedCode,
-//       });
+    console.log("signUp.id", signUp?.id);
+    // return;
 
-//       if (signUpAttempt.status === "complete") {
-//         await setActive({ session: signUpAttempt.createdSessionId });
-//       } else {
-//         Alert.alert("Error", "Verification failed, please try again.");
-//         console.error(JSON.stringify(signUpAttempt, null, 2));
-//       }
-//     } catch (err) {
-//       Alert.alert("Error", err.errors?.[0]?.message || "Verification Failed");
-//       console.error(JSON.stringify(err, null, 2));
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
+    try {
+      const signUpAttempt = await signUp.attemptEmailAddressVerification({
+        code: trimmedCode,
+      });
 
-//   return (
-//     <View style={authStyles.container}>
-//       <KeyboardAvoidingView
-//         behavior={Platform.OS === "ios" ? "padding" : "height"}
-//         keyboardVerticalOffset={Platform.OS === "ios" ? 64 : 0}
-//         style={authStyles.keyboardView}
-//       >
-//         <ScrollView
-//           contentContainerStyle={authStyles.scrollContent}
-//           showsVerticalScrollIndicator={false}
-//         >
-//           <View style={authStyles.imageContainer}>
-//             <Image
-//               source={require("../../assets/images/i3.png")}
-//               style={authStyles.image}
-//               contentFit="contain"
-//             />
-//           </View>
-//           <Text style={authStyles.title}>Verify Your Account</Text>
-//           <Text style={authStyles.subtitle}>
-//             We've have sent a Verification code to {email}
-//           </Text>
+      if (signUpAttempt.status === "complete") {
+        await setActive({ session: signUpAttempt.createdSessionId });
+      } else {
+        Alert.alert("Error", "Verification failed, please try again.");
+        console.error(JSON.stringify(signUpAttempt, null, 2));
+      }
+    } catch (err) {
+      Alert.alert("Error", err.errors?.[0]?.message || "Verification Failed");
+      console.error(JSON.stringify(err, null, 2));
+    } finally {
+      setLoading(false);
+    }
+  };
 
-//           {/* Form container */}
-//           <View style={authStyles.formContainer}>
-//             {/* email input */}
-//             <View style={authStyles.inputContainer}>
-//               <TextInput
-//                 style={authStyles.textInput}
-//                 placeholder="Enter verification code"
-//                 placeholderTextColor={COLORS.textLight}
-//                 value={code}
-//                 // onChange={setEmail}
-//                 onChangeText={(text) => setCode(text)}
-//                 keyboardType="number-pad"
-//                 autoCapitalize="none"
-//               />
-//             </View>
+  return (
+    <View style={authStyles.container}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        keyboardVerticalOffset={Platform.OS === "ios" ? 64 : 0}
+        style={authStyles.keyboardView}
+      >
+        <ScrollView
+          contentContainerStyle={authStyles.scrollContent}
+          showsVerticalScrollIndicator={false}
+        >
+          <View style={authStyles.imageContainer}>
+            <Image
+              source={require("../../assets/images/i3.png")}
+              style={authStyles.image}
+              contentFit="contain"
+            />
+          </View>
+          <Text style={authStyles.title}>Verify Your Account</Text>
+          <Text style={authStyles.subtitle}>
+            We've have sent a Verification code to {email}
+          </Text>
 
-//             <TouchableOpacity
-//               style={[
-//                 authStyles.authButton,
-//                 loading && authStyles.buttonDisabled,
-//               ]}
-//               onPress={handleVerification}
-//               disabled={loading}
-//               activeOpacity={0.8}
-//             >
-//               <Text style={authStyles.buttonText}>
-//                 {loading ? "Verifying..." : "Verify Email"}
-//               </Text>
-//             </TouchableOpacity>
+          {/* Form container */}
+          <View style={authStyles.formContainer}>
+            {/* email input */}
+            <View style={authStyles.inputContainer}>
+              <TextInput
+                style={authStyles.textInput}
+                placeholder="Enter verification code"
+                placeholderTextColor={COLORS.textLight}
+                value={code}
+                // onChange={setEmail}
+                onChangeText={(text) => setCode(text)}
+                keyboardType="number-pad"
+                autoCapitalize="none"
+              />
+            </View>
 
-//             {/* Sign up Link */}
-//             <TouchableOpacity style={authStyles.linkContainer} onPress={onBack}>
-//               <Text style={authStyles.linkText}>
-//                 Back to sign up <Text style={authStyles.link}>Sign in</Text>
-//               </Text>
-//             </TouchableOpacity>
-//           </View>
-//         </ScrollView>
-//       </KeyboardAvoidingView>
-//     </View>
-//   );
-// };
+            <TouchableOpacity
+              style={[
+                authStyles.authButton,
+                loading && authStyles.buttonDisabled,
+              ]}
+              onPress={handleVerification}
+              disabled={loading}
+              activeOpacity={0.8}
+            >
+              <Text style={authStyles.buttonText}>
+                {loading ? "Verifying..." : "Verify Email"}
+              </Text>
+            </TouchableOpacity>
 
-// export default VerifyEmail;
+            {/* Sign up Link */}
+            <TouchableOpacity style={authStyles.linkContainer} onPress={onBack}>
+              <Text style={authStyles.linkText}>
+                Back to sign up <Text style={authStyles.link}>Sign in</Text>
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </View>
+  );
+};
+
+export default VerifyEmail;
